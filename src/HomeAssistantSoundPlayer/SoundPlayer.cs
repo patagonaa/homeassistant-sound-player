19ee1a44ea81d4ac9576f6768a2bfc2b8eb76daf
Patrick Krämer <patagonahn@gmail.com>
2020-10-16 15:55:05 +0200

    add discovery submosule

--- src/HomeAssistantSoundPlayer/SoundPlayer.cs
+++ src/HomeAssistantSoundPlayer/SoundPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using HomeAssistantDiscoveryHelper;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MQTTnet;
